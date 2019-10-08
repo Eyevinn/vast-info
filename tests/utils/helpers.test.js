430635a2d2a686c129957f23ad12512b7b59fd06
Erik Hoffman <erik.johansson87@gmail.com>
2019-10-08 23:29:06 +0200

    added some more tests for helper file

--- tests/utils/helpers.test.js
+++ tests/utils/helpers.test.js
@@ -1,20 +1,30 @@
-require("jest");
 const helpers = require("../../utils/helpers");
 
 describe("helpers", () => {
   describe("get", () => {
-    it("should response with deep object value if available", () => {
-      const obj = {
-        "Fruit Basket": {
-          fruits: {
-            banana: "yellow",
-            orange: "orange",
-            apple: "red"
-          }
+    const obj = {
+      "Fruit Basket": {
+        fruits: {
+          banana: "yellow",
+          orange: "orange",
+          apple: "red"
         }
-      };
+      }
+    };
+    it("should response with deep object value if available", () => {
       const val = helpers.get(obj, "Fruit Basket.fruits.banana");
       expect(val).toBe("yellow");
+    });
+    it("should response with undefined if value isn't available", () => {
+      const val = helpers.get(obj, "Fruit Basket.fruits.pear");
+      expect(val).toBeUndefined();
+    });
+  });
+
+  describe("durationAsSeconds", () => {
+    it("should return the number representation of given time string", () => {
+      expect(helpers.durationAsSeconds("00:00:20")).toEqual(20);
+      expect(helpers.durationAsSeconds("00:01:00")).toEqual(60);
     });
   });
 });
